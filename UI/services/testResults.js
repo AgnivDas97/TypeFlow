2b8298785c59b55c9005d8c9c497bb5a0303a979
Agniv Das <132659204+AgnivDas97@users.noreply.github.com>
2025-11-10 01:42:07 +0530

    Update API_URL to use environment variable

--- UI/services/testResults.js
+++ UI/services/testResults.js
@@ -1,6 +1,7 @@
 import { getToken } from "./authService";
-const API_URL = "http://localhost:5000/api/tests";
+// const API_URL = "http://localhost:5000/api/tests";
 
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const saveTestResult = async (resultData) => {
   const token = await getToken();
